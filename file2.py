162ca7bac4c9ba7c4fefdd0c2751c27765a39468
poorvik <poorvipoorvikbs@gmail.com>
2026-09-07 23:08:21 +0530

    changes

--- file2.py
+++ file2.py
@@ -4,3 +4,10 @@ if num==num[::-1]:
     print("palindrome")
 else:
     print("not a palindrome")
+
+num=int(input("enter number:"))
+fact=1
+
+for i in range(1,n+1):
+    fact=fact*i
+print("fact")
